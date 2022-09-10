5f6b5123fe32b145b9730d7699cf7d674ae6d625
Jae Gifford <jae.giff@outlook.com>
2022-09-10 11:40:59 +0800

    current tests update

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,1 +1,5 @@
 import analyzeArray from './analyzeArray';
+
+test('Does it give avg, max min and length as an object?', () => {
+  expect(analyzeArray().toBe(''));
+});
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,1 +1,5 @@
 import capitalize from './capitalize';
+
+test('Does it capitalize the first letter?', () => {
+  expect(capitalize('hello')).toBe('Hello');
+});
